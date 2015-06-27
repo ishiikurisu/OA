6e0cd98a4c22d833b2216aba7285ec78fa3f6213
Cris Silva Jr <cristianoalvesjr@gmail.com>
2015-06-27 13:45:31 -0300

    Added new hash functions

--- hash/src/hashfunctions.h
+++ hash/src/hashfunctions.h
@@ -1,3 +1,6 @@
+#include <math.h>
+#include <limits.h>
+
 int stupid(char* input)
 {
     int output = 0;
@@ -20,4 +23,34 @@ int dumb(char* input)
         output += input[i];
 
     return (output << 2) + (output >> 2);
+}
+
+int knuth(char* input)
+{
+    int result = 0;
+    int limit = strlen(input);
+    int i = 0;
+
+    for (i = 0; i < limit; ++i)
+        result = (result + input[i] * 7) % (INT_MAX);
+    result *= 3557;
+    result = abs(result);
+
+    return result;
+}
+
+int MD5(char* input)
+{
+    int result = 0;
+    int limit = strlen(input);
+    int constant = 0;
+    int i = 0;
+
+    for (i = 0; i < limit; ++i)
+    {
+        constant += (int) abs(sin(i + 1) * INT_MAX);
+        result = result * 10 + input[i] * constant;
+    }
+
+    return result;
 }
