4324c53d4e4ace89b0770bf5f66fd049ef968421
Cris Silva Jr <ishiikurisu@yahoo.com>
2015-10-30 09:38:44 -0200

    Began thinking about how to merge index files

--- T1/src/intercalar-indices.hpp
+++ T1/src/intercalar-indices.hpp
@@ -4,20 +4,37 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <criador-indices-primario.hpp>
 
 class IntercaladorIndices
 {
+	void preparar(std::string);
 public:
 	IntercaladorIndices() {};
 	void intercalar(std::string, std::string);
 };
 
+/*******************
+* FUNÇÕES PRIVADAS *
+*******************/
+
+void IntercaladorIndices::preparar(std::string endereco)
+{
+
+}
+
+/*******************
+* FUNÇÕES PÚBLICAS *
+*******************/
+
 void IntercaladorIndices::intercalar(std::string arquivo1, std::string arquivo2)
 {
+	std::string s1;
+	std::string s2;
 	std::fstream fs;
 	fs.open("lista12.txt");
 
-	
+	preparar();
 
 	fs.close();
 }
