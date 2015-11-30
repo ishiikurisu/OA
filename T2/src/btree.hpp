23c00299caa98ce221be9914f4be6ee5810d634e
Cris Silva Jr <ishiikurisu@yahoo.com>
2015-11-30 18:53:23 -0200

    Converting spaces to tabs

--- T2/src/btree.hpp
+++ T2/src/btree.hpp
@@ -11,16 +11,16 @@
 
 class BTree
 {
-    std::vector<std::string> enderecos;
-    Pagina raiz;
-    unsigned int numero_paginas;
-    void mostrar(Pagina*);
+	std::vector<std::string> enderecos;
+	Pagina raiz;
+	unsigned int numero_paginas;
+	void mostrar(Pagina*);
 public:
-    BTree();
-    void adicionar(std::string, unsigned int);
-    void adicionar(Node);
-    void mostrar();
-    friend class Pagina;
+	BTree();
+	void adicionar(std::string, unsigned int);
+	void adicionar(Node);
+	void mostrar();
+	friend class Pagina;
 };
 
 /*******************
@@ -29,15 +29,17 @@ public:
 
 void BTree::mostrar(Pagina* pagina)
 {
+    
+
     std::cout << "pagina: " << pagina->no_pagina << std::endl;
-    std::cout << "dados:" << std::endl;
-    if (pagina->filhas.size() > 0)
-    {
-        std::cout << "filhas:" << std::endl;
-        for (std::vector<unsigned int> filha = pagina->filhas.size();
-             )
-    }
-    std::cout << std::endl;
+	std::cout << "dados:" << std::endl;
+	for ()
+	if (pagina->filhas.size() > 0)
+	{
+		std::cout << "filhas:" << std::endl;
+
+	}
+	std::cout << std::endl;
 }
 
 /*******************
@@ -46,33 +48,33 @@ void BTree::mostrar(Pagina* pagina)
 
 BTree::BTree()
 {
-    raiz.definir_pagina(1);
-    raiz.definir_mae(-1);
+	raiz.definir_pagina(1);
+	raiz.definir_mae(-1);
 }
 
 void BTree::adicionar(Node no)
 {
-    Pagina *filha = raiz.achar_filha(no);
-    filha->adicionar(no);
+	Pagina *filha = raiz.achar_filha(no);
+	filha->adicionar(no);
 
-    while (filha->overflow())
-    {
-        no = filha->dividir_filhas();
-        filha = filha->lidar_com_mae();
-        filha->adicionar(no);
-    }
+	while (filha->overflow())
+	{
+		no = filha->dividir_filhas();
+		filha = filha->lidar_com_mae();
+		filha->adicionar(no);
+	}
 }
 
 void BTree::adicionar(std::string dado, unsigned int no_linha)
 {
-    adicionar(Node(dado, no_linha));
+	adicionar(Node(dado, no_linha));
 }
 
 void BTree::mostrar()
 {
-    std::cout << "--- # BTREE" << std::endl;
-    this->mostrar(&raiz);
-    std::cout << "..." << std::endl;
+	std::cout << "--- # BTREE" << std::endl;
+	this->mostrar(&raiz);
+	std::cout << "..." << std::endl;
 }
 
 #endif /* end of include guard: BTREE_H */
